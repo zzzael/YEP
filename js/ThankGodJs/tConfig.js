8c3dfa924d9bbc8d54f272751be5a89eb6ea9a0d
zzzael <mensahdaniel279@gmail.com>
2024-08-25 19:54:28 +0100

    Update tConfig.js

--- js/ThankGodJs/tConfig.js
+++ js/ThankGodJs/tConfig.js
@@ -1,23 +1,16 @@
-// v2config.js
+// tConfig.js
 
 // Updated track list with songs from the image
 const trackList = [
-    { name: "AWESOME", artist: "Kanye West (feat. Charlie Wilson)", src: "music/VULTURES_II/1_SLIDIN.mp3" },
-    { name: "One I Love", artist: "Kanye West (feat. The Mama & Papas", src: "music/VULTURES_II/2_TIME_MOVING_SLOW.mp3" },
-    { name: "Cant Get Over Me", artist: "Justin Vernon, Tony Will...", src: "music/VULTURES_II/3_FIELD_TRIP.mp3" },
-    { name: "Shy Can't Look", artist: "Kanye West (feat. John Legends)", src: "music/VULTURES_II/4_TAKE_OFF_YOUR_DRESS.mp3" },
-    { name: "No More Games - Interl...", artist: "Kanye West", src: "music/VULTURES_II/5_DRUNK.mp3" },
-    { name: "ISABELLA", artist: "Kanye West", src: "music/VULTURES_II/6_ISABELLA.mp3" },
-    { name: "PROMOTION", artist: "Kanye West", src: "music/VULTURES_II/7_PROMOTION.mp3" },
-    { name: "LIFESTYLE", artist: "Kanye West", src: "music/VULTURES_II/8_LIFESTYLE.mp3" },
-    { name: "SO CALLED FRIENDS", artist: "Kanye West", src: "music/VULTURES_II/9_SO_CALLED_FRIENDS.mp3" },
-    { name: "BELIEVER", artist: "Kanye West", src: "music/VULTURES_II/10_BELIEVER.mp3" },
-    { name: "MELROSE", artist: "Kanye West", src: "music/VULTURES_II/11_MELROSE.mp3" },
-    { name: "FOREVER ROLLIN", artist: "Kanye West", src: "music/VULTURES_II/12_FOREVER_ROLLIN.mp3" },
-    { name: "DEAD", artist: "Kanye West", src: "music/VULTURES_II/13_DEAD.mp3" },
-    { name: "GUN TO MY HEAD", artist: "Kanye West", src: "music/VULTURES_II/14_GUN_TO_MY_HEAD.mp3" },
-    { name: "RIVER", artist: "Kanye West", src: "music/VULTURES_II/15_RIVER.mp3" },
-    { name: "MY SOUL", artist: "Kanye West", src: "music/VULTURES_II/16_MY_SOUL.mp3" }
+    { name: "AWESOME", artist: "Kanye West (feat. Charlie Wilson)", src: "music/TGFD/Awesome.mp3" },
+    { name: "One I Love", artist: "Kanye West (feat. The Mama & Papas)", src: "music/TGFD/One_I_Love.mp3" },
+    { name: "Cant Get Over Me", artist: "Justin Vernon, Tony Will...", src: "music/TGFD/CantGetover.mp3" },
+    { name: "Shy Can't Look", artist: "Kanye West (feat. John Legends)", src: "music/TGFD/ShyCantLook.mp3" },
+    { name: "No More Games - Interlude", artist: "Kanye West (feat. Chantal Kreviazuk)", src: "music/TGFD/NoMoreGames.mp3" },
+    { name: "Imitation of Christ", artist: "Kanye West", src: "music/TGFD/ImitationOfChrist.mp3" },
+    { name: "No No No", artist: "Kanye West (feat. 2 Chainz, The-Dream & Travis Scott)", src: "music/TGFD/7_PROMOTION.mp3" },
+    { name: "I Am Not Home", artist: "Kanye West", src: "music/TGFD/IamNotHome.mp3" }
+
 ];
 // THANK GOD FOR DRUGS (prod. Dr. UnReleased x Petah)
 // 1. Awesome (feat. Charlie Wilson)
